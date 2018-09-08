ebaf9bec6bdcc6e70feb7592407a1dcc9a53ef07
Syed Ahsan Ahmed <snahmed1998@gmail.com>
2018-09-08 14:25:44 +0500

    1d array names

--- MultipleForms-lab2/MultipleForms-lab2/Form1.cs
+++ MultipleForms-lab2/MultipleForms-lab2/Form1.cs
@@ -12,6 +12,8 @@ namespace MultipleForms_lab2
 {
     public partial class Form1 : Form
     {
+        string[] names = { "Ahsan", "Mohsin", "Zakir" };
+
         public Form1()
         {
             InitializeComponent();
@@ -27,14 +29,61 @@ namespace MultipleForms_lab2
 
         private void selectCountry_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+            if (selectCountry.SelectedItem == "Pakistan")
+            {
+                citySelect.Items.Clear();
+                citySelect.Items.Add("Karachi");
+                citySelect.Items.Add("Lahore");
+                citySelect.Items.Add("Islamabad");
+                citySelect.Items.Add("Rawalpindi");
+                citySelect.Items.Add("Quetta");
+            }
+            if (selectCountry.SelectedItem == "China")
+            {
+                citySelect.Items.Clear(); //comment
+                citySelect.Items.Add("Beijing");
+                citySelect.Items.Add("Shanghai");
+                citySelect.Items.Add("Hong Kong");
+            }
+            if (selectCountry.SelectedItem == "India")
+            {
+                citySelect.Items.Clear();
+                citySelect.Items.Add("Mumbai");
+                citySelect.Items.Add("New Delhi");
+                citySelect.Items.Add("Bhopal");
+                citySelect.Items.Add("Lucknow");
+            }
+            if (selectCountry.SelectedItem == "Afghanistan")
+            {
+                citySelect.Items.Clear();
+                citySelect.Items.Add("Kabul");
+                citySelect.Items.Add("Kunduz");
+                citySelect.Items.Add("Ghazni");
+            }
         }
 
         private void selectCity_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (selectCountry.SelectedItem == "Afghanistan")
+            
+        }
+
+        private void closeBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            if (firstnameTxt.Text != "")
             {
-                selectCity.Items.Add("Kabul");
+                for (int i = 0; i < names.Length; i++)
+                {
+                   if (firstnameTxt.Text == names[i])
+                    {
+                        searchResult.Items.Clear();
+                        searchResult.Items.Add(names[i][0]);
+                    }
+                }
             }
         }
     }
